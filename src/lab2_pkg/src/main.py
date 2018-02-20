43c75edf02dbe2f87036a8073f35dbc3e230ed36
Henk Tillman <henktillman@gmail.com>
2018-02-20 15:03:54 -0800

    documentation

--- src/lab2_pkg/src/main.py
+++ src/lab2_pkg/src/main.py
@@ -6,6 +6,7 @@ Author: Chris Correa
 import numpy as np
 import math
 import sys
+import pdb
 
 import rospy
 import tf
@@ -30,70 +31,34 @@ import cvxpy as cvx
 import Queue
 from grasp_metrics import compute_force_closure, compute_gravity_resistance, compute_custom_metric
 
-# probably don't need to change these (but confirm that they're correct)
-MAX_HAND_DISTANCE = .04
-MIN_HAND_DISTANCE = .01
-CONTACT_MU = 0.5
-CONTACT_GAMMA = 0.1
-
-# will need to change these
-OBJECT_MASS = ??? # kg
-# approximate the friction cone as the linear combination of `NUM_FACETS` vectors
-NUM_FACETS = 32
-# set this to false while debugging your grasp analysis
-BAXTER_CONNECTED = False
-# how many to execute
-NUM_GRASPS = 6
-OBJECT = "spray"
-
-# objects are different this year so you'll have to change this
-# also you can use nodes/object_pose_publisher.py instead of finding the ar tag and then computing T_ar_object in this script.
-if OBJECT == "spray":
-    MESH_FILENAME = 'objects/spray.obj'
-    # ar tag on the paper
-    TAG = 8
-    # transform between the object and the AR tag on the paper
-    T_ar_object = tfs.translation_matrix([-.09, -.065, 0.106])
-    # how many times to subdivide the mesh
-    SUBDIVIDE_STEPS = 0
-elif OBJECT == 'bar_clamp':
-    MESH_FILENAME = 'objects/bar_clamp.obj'
-    TAG = 9
-    T_ar_object = tfs.translation_matrix([-.09, -.065, 0.035])
-    SUBDIVIDE_STEPS = 1
-elif OBJECT == "mount2":
-    MESH_FILENAME = 'objects/mount2.obj'
-    TAG = 10
-    T_ar_object = tfs.translation_matrix([-.09, -.065, 0.038])
-    SUBDIVIDE_STEPS = 0
-
-if BAXTER_CONNECTED:
-    right_gripper = baxter_gripper.Gripper('right')
-
-listener = tf.TransformListener()
-from_frame = 'base'
-time.sleep(1)
+"""
+Starter Commands
+rosrun baxter_tools enable_robot.py -e
+rosrun baxter tools camera control.py -o left hand camera -r 1280x800
+rosrun baxter tools camera control.py -o right hand camera -r 1280x800
+roslaunch lab2 pkg baxter left hand track.launch
+rosrun baxter_interface joint_trajectory_action_server.py
+roslaunch baxter_moveit_config demo_baxter.launch right_electric_gripper:=true left_electric_gripper:=true
+    Only if you need MoveIt
+rosrun lab2_pkg main.py
+"""
 
 def lookup_tag(tag_number):
-    """ Returns the AR tag position in world coordinates 
-
-    Parameters
-    ----------
-    tag_number : int
-        AR tag number
-
-    Returns
-    -------
-    :obj:`autolab_core.RigidTransform` AR tag position in world coordinates
-    """
+    listener = tf.TransformListener()
+    from_frame = 'base'
     to_frame = 'ar_marker_{}'.format(tag_number)
-    if not listener.frameExists(from_frame) or not listener.frameExists(to_frame):
-        print 'Frames not found'
-        print 'Did you place AR marker {} within view of the baxter left hand camera?'.format(tag_number)
-        exit(0)
-    t = listener.getLatestCommonTime(from_frame, to_frame)
-    tag_pos, tag_rot = listener.lookupTransform(from_frame, to_frame, t)
-    return rigid_transform(tag_pos, tag_rot)
+    while not rospy.is_shutdown():
+        try:
+            # if not listener.frameExists(from_frame) or not listener.frameExists(to_frame):
+            #     print 'Frames not found'
+            #     print 'Did you place AR marker {} within view of the baxter left hand camera?'.format(tag_number)
+            #     exit(0)
+            t = listener.getLatestCommonTime(from_frame, to_frame)
+            tag_pos, _ = listener.lookupTransform(from_frame, to_frame, t)
+            break
+        except:
+            continue
+    return tag_pos[0:3] # only return x, y, z
 
 def close_gripper():
     """closes the gripper"""
@@ -130,6 +95,8 @@ def execute_grasp(T_object_gripper):
     inp = raw_input('Press <Enter> to move, or \'exit\' to exit')
     if inp == "exit":
         return
+    # moveit go_to_pose()
+    # change the planner used with set_planner_id() in main
     # YOUR CODE HERE
 
 def contacts_to_baxter_hand_pose(contact1, contact2, approach_direction):
@@ -197,6 +164,51 @@ def sorted_contacts(vertices, normals, T_ar_object):
     return grasp_indices, best_metric_indices
 
 
+# probably don't need to change these (but confirm that they're correct)
+MAX_HAND_DISTANCE = .04
+MIN_HAND_DISTANCE = .01
+CONTACT_MU = 0.5 # coefficient of friction
+CONTACT_GAMMA = 0.1 # coefficient of tortion friction
+
+# will need to change these
+OBJECT_MASS = 0.25 # kg
+# approximate the friction cone as the linear combination of `NUM_FACETS` vectors
+NUM_FACETS = 32
+# set this to false while debugging your grasp analysis
+BAXTER_CONNECTED = False
+# how many to execute
+NUM_GRASPS = 5
+OBJECT = "pawn"
+
+# objects are different this year so you'll have to change this
+# also you can use nodes/object_pose_publisher.py instead of finding the ar tag and then computing T_ar_object in this script.
+if OBJECT == "pawn":
+    MESH_FILENAME = 'objects/pawn.obj'
+    # ar tag on the paper
+    TAG = 8
+    # transform between the object and the AR tag on the paper
+    T_ar_object = tfs.translation_matrix([-.09, -.065, 0.106])
+    # how many times to subdivide the mesh
+    SUBDIVIDE_STEPS = 0
+elif OBJECT == 'nozzle':
+    MESH_FILENAME = 'objects/nozzle.obj'
+    TAG = 9
+    T_ar_object = tfs.translation_matrix([-.09, -.065, 0.035])
+    SUBDIVIDE_STEPS = 1
+elif OBJECT == "gearbox":
+    MESH_FILENAME = 'objects/gearbox.obj'
+    TAG = 10
+    T_ar_object = tfs.translation_matrix([-.09, -.065, 0.038])
+    SUBDIVIDE_STEPS = 0
+
+# Change to left if necessary!
+if BAXTER_CONNECTED:
+    right_gripper = baxter_gripper.Gripper('right')
+
+listener = tf.TransformListener()
+from_frame = 'base'
+time.sleep(1)
+
 if __name__ == '__main__':
     if BAXTER_CONNECTED:
         moveit_commander.roscpp_initialize(sys.argv)
@@ -224,24 +236,24 @@ if __name__ == '__main__':
     triangles = mesh.triangles
     normals = mesh.normals
 
-    ??? = sorted_contacts(???)
+    # ??? = sorted_contacts(???)
 
-    # YOUR CODE HERE
-    for current_metric in ?????:
-        # YOUR CODE HERE
+    # # YOUR CODE HERE
+    # for current_metric in ?????:
+    #     # YOUR CODE HERE
         
-        # visualize the mesh and contacts
-        vis.figure()
-        vis.mesh(mesh)
-        vis.normals(NormalCloud(np.hstack((normal1.reshape(-1, 1), normal2.reshape(-1, 1))), frame='test'),
-            PointCloud(np.hstack((contact1.reshape(-1, 1), contact2.reshape(-1, 1))), frame='test'))
-        # vis.pose(T_obj_gripper, alpha=0.05)
-        vis.show()
-        if BAXTER_CONNECTED:
-            repeat = True
-            while repeat:
-                execute_grasp(T_obj_gripper)
-                repeat = bool(raw_input("repeat?"))
+    #     # visualize the mesh and contacts
+    #     vis.figure()
+    #     vis.mesh(mesh)
+    #     vis.normals(NormalCloud(np.hstack((normal1.reshape(-1, 1), normal2.reshape(-1, 1))), frame='test'),
+    #         PointCloud(np.hstack((contact1.reshape(-1, 1), contact2.reshape(-1, 1))), frame='test'))
+    #     # vis.pose(T_obj_gripper, alpha=0.05)
+    #     vis.show()
+    #     if BAXTER_CONNECTED:
+    #         repeat = True
+    #         while repeat:
+    #             execute_grasp(T_obj_gripper)
+    #             repeat = bool(raw_input("repeat?"))
 
     # 500, 1200
     exit()
